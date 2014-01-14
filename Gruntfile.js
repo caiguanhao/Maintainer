05e36624872d118ddd001fa0f0cc6c9582ab94b9
Choi Goon-ho <caiguanhao@gmail.com>
2014-01-14 13:15:37 +0800

    first commit;

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      options: {
+        livereload: true
+      },
+      gruntfile: {
+        files: [ 'Gruntfile.js' ]
+      },
+      js: {
+        files: [ 'assets/js/**/*.js', 'public/**/*.html' ]
+      },
+      server: {
+        files: [ '<%= pkg.main %>' ],
+        tasks: [ 'develop' ],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+    develop: {
+      server: {
+        file: '<%= pkg.main %>'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-develop');
+
+  grunt.registerTask('default', [ 'develop', 'watch' ]);
+
+};
---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', 3000);
+
+app.use(express.static(__dirname + '/assets'));
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
